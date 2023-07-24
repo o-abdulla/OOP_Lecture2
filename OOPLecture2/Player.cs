f788e6b177bb29800b7150d7d1090936f67359ee
Omar Abdulla <osabdulla@gmail.com>
2023-07-24 17:04:02 -0400

    First

---
+++ OOPLecture2/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLecture2
+{
+    internal class Player
+    {
+        //properties
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public int Level { get; set; }
+        //constructor
+        public Player(string _username, string _role, int _level)
+        {
+            Username = _username;
+            Role = _role;
+            Level = _level;
+        }
+        //overloading
+        public Player()
+        {
+            Username = "New User";
+            Role = "Unassigned";
+            Level = 1;
+        }
+        //methods
+        public string GetDetails()
+        {
+            return $"Username: {Username} Role: {Role} Level: {Level}";
+        }
+    }
+}
